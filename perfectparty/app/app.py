ce6d01e462844661578e80f3aa7bb0e3ba04e5dc
Cynthia Shu <c8shu@uwaterloo.ca>
2019-07-21 17:59:54 -0400

    CORS

--- perfectparty/app/app.py
+++ perfectparty/app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 
 if __name__ == '__main__':
     app = Flask(__name__)
     api = Api(app)
+
+    CORS(app)
 
     from extensions import DatabaseConnection
     DatabaseConnection('host=db port=5432 dbname=postgres user=postgres password=')
