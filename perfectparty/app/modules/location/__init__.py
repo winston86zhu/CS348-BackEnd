7be7b0e05e8bb4c05606720d8a02812f58dae8a1
Andrew Tam <andrewjosephtam@gmail.com>
2019-07-27 14:58:39 -0400

    update location by id (#11)

--- perfectparty/app/modules/location/__init__.py
+++ perfectparty/app/modules/location/__init__.py
@@ -3,3 +3,4 @@ def init_api(api):
 
     from . import resources
     api.add_resource(resources.LocationResource, f'{base_url}')
+    api.add_resource(resources.SpecificLocationResource, f'{base_url}/<int:location_id>')
--- perfectparty/app/modules/location/managers.py
+++ perfectparty/app/modules/location/managers.py
@@ -9,11 +9,44 @@ class LocationManager(db_conn):
     def deserialize(self, row):
         return Location(*row)
 
+    def fetch_by_id(self, location_id):
+        query = f"""
+            SELECT *
+            FROM Location
+            WHERE LocationID = {location_id}
+        """
+
+        try:
+            result = self.fetch_single_row(query)
+        except Exception as e:
+            self.rollback()
+            raise e
+
+        return self.deserialize(result)
+
     def create(self, location):
         query = f"""
             INSERT 
             INTO Location (LocationCapacity, LocationOpenHour, LocationCloseHour, LocationName, LocationAddress, LocationPrice)
             VALUES({location.location_capacity}, {location.location_open_hour}, {location.location_close_hour}, '{location.location_name}', '{location.location_address}', {location.location_price});
+        """
+
+        try:
+            self.execute_write_op(query)
+        except Exception as e:
+            self.rollback()
+            raise e
+
+    def update(self, location):
+        query = f"""
+            UPDATE Location
+            SET LocationCapacity = '{location.location_capacity}',
+                LocationOpenHour = '{location.location_open_hour}',
+                LocationCloseHour = '{location.location_close_hour}',
+                LocationName = '{location.location_name}',
+                LocationAddress = '{location.location_address}',
+                LocationPrice = '{location.location_price}'
+            WHERE LocationID = {location.location_id};
         """
 
         try:
