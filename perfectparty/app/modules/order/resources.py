ed179ef6a9c3cda7a3c1b1b95b143ec43f0d307a
Andrew Tam <ahndrewtam@gmail.com>
2019-07-27 17:17:22 -0400

    delete order

--- perfectparty/app/modules/order/resources.py
+++ perfectparty/app/modules/order/resources.py
@@ -62,3 +62,34 @@ class OrderResource(Resource):
         response = jsonify(result)
         response.status_code = 201
         return response
+
+    @staticmethod
+    def delete():
+        payload = request.get_json()
+
+        try:
+            item_id = payload['item_id']
+            supplier_user_id = payload['supplier_user_id']
+            client_user_id = payload['client_user_id']
+            event_id = payload['event_id']
+        except KeyError as e:
+            response = jsonify({
+                'error': 'KeyError',
+                'message': f'Missing key {str(e)} from payload'
+            })
+            response.status_code = 400
+            return response
+
+        manager = OrderManager()
+
+        try:
+            manager.delete(item_id, supplier_user_id, client_user_id, event_id)
+        except Exception as e:
+            response = jsonify({
+                'error': 'Internal Error',
+                'message': f'Unknown error: {str(e)}'
+            })
+            response.status_code = 500
+            return response
+
+        return '', 204
